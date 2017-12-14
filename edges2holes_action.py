c42d6b75f4cee72b1d118ce6104ece8cba1ee4c1
Tom O'Connell <toconnel@caltech.edu>
2017-12-14 14:54:36 -0800

    Think ceiling has holes converted to a manufacturable format. Going to do floor now then regenerate output for manufacture.

--- edges2holes_action.py
+++ edges2holes_action.py
@@ -28,6 +28,8 @@ class HolePlugin(pcbnew.ActionPlugin):
         radii = set()
         circles = []
 
+        # TODO don't add an extra hole in same place -> overwrite
+
         # get circles drawn on board
         # TODO maybe include some means of filtering out possible circular board outline
         drawing_list = board.DrawingsList()
@@ -44,6 +46,10 @@ class HolePlugin(pcbnew.ActionPlugin):
                     c = d.GetCenter()
                     circles.append((c.x, c.y, r))
 
+                    # TODO make optional
+                    # how to do?
+                    board.RemoveNative(d)
+
        
         # TODO option to use nearest hole in existing library option / next largest
         # (report error)
@@ -57,8 +63,8 @@ class HolePlugin(pcbnew.ActionPlugin):
 
         # TODO include field for library to be saved to (maybe mandatory if this option?)
         # TODO fail gracefully if board filename is empty (board not saved yet)
-        target_footprint_lib = '.'.join(str(board.GetFileName()).split('.')[-1] \
-            + ['.pretty'])
+        target_footprint_lib = '.'.join(str(board.GetFileName()).split('.')[:-1]) \
+            + '.pretty'
 
         if not os.path.exists(target_footprint_lib):
             print('creating project specific footprint library' + \
@@ -74,6 +80,14 @@ class HolePlugin(pcbnew.ActionPlugin):
             print('generating footprint for hole of radius {}mm'.format(r_mm))
 
             footprint = pcbnew.MODULE(None)
+            # for non-electronic parts like these
+            footprint.SetAttributes(pcbnew.MOD_VIRTUAL)
+            footprint.SetLastEditTime()
+            footprint.SetKeywords('non-plated through hole NPTH')
+
+            # TODO it looks like the module itself needs to be specified to be on F.Cu?
+            # how? (see github for example output)
+
             # TODO set tags to include non-plated through hole, npth?
             #footprint.SetDescription('{}mm diameter non-plated through hole')
             pad = pcbnew.D_PAD(footprint)
@@ -83,7 +97,7 @@ class HolePlugin(pcbnew.ActionPlugin):
 
             # TODO does this change the layers and stuff automatically? pad shape?
             pad.SetAttribute(pcbnew.PAD_ATTRIB_HOLE_NOT_PLATED)
-            pad.SetShape(pcbnew.CIRCLE)
+            pad.SetShape(pcbnew.PAD_SHAPE_CIRCLE)
             # TODO do i need setsize / setstartend?
             # size seems to equal (r, r) for existing npth library parts
 
@@ -91,14 +105,16 @@ class HolePlugin(pcbnew.ActionPlugin):
             # copper layers (F.Cu should be one of these layers. mask the other?)
             pad.SetLayerSet(pcbnew.D_PAD_UnplatedHoleMask())
 
-            # TODO what is attr virtual and why do all library M2s holes have them?
             # TODO are holes alone actually rendered, or do you need the other stuff?
 
             # there seems to be a PAD_DRILL_SHAPE_CIRCLE and PAD_DRILL_SHAPE_OBLONG
             # but when are they ever not circular? oblong = limited routing?
             # would advanced circuits do oblong, for instance?
             #pad.SetDrillShape() # huh???
-            pad.SetDrillSize(pcbnew.wxSize(r))
+            # TODO why isn't this a single number? what gens (drill X)?
+            # TODO why is x in (size x x) slightly larger than 1.5?
+            pad.SetDrillSize(pcbnew.wxSize(r, r))
+            footprint.Add(pad)
 
             footprint_id = 'R{}mm_NPTH'.format(r_mm)
             footprint.SetReference(footprint_id)
@@ -121,36 +137,31 @@ class HolePlugin(pcbnew.ActionPlugin):
             footprint.SetLocked(True)
 
             print('saving footprint to {}'.format(os.path.join(\
-                target_footprint_lib, footprint_id + '.kicad_mod'))
+                target_footprint_lib, footprint_id + '.kicad_mod')))
             pcbnew.PCB_IO().FootprintSave(target_footprint_lib, footprint)
 
-            f = pcbnew.PCB_IO().FootprintLoad(target_footprint_lib, fpid)
             for x, y, r_curr in circles:
                 if r == r_curr:
-                    # TODO what works here? clone? duplicate seems wrong
-                    f_new = f.Clone()
-                    # TODO maybe setposition?
-                    f_new.SetCenter(pcbnew.wxPoint(c.x, c,y))
-                    # TODO everything set right?
-                    board.AddNative(f_new)
-
-                    # TODO optionally delete existing hole outlines
-
-        # TODO make library if necessary (?) (way to check for existing project specific library?)
-        # or clean way to add a library? (rather than manually editing the files to add lines)
+                    f_new = pcbnew.PCB_IO().FootprintLoad(target_footprint_lib, footprint_id)
+                    f_new.SetPosition(pcbnew.wxPoint(x, y))
+                    board.Add(f_new)
 
         # TODO convert slots / particularly large holes to a series of holes?
         # TODO maybe offer a (np) pilot hole for particularly large holes?
 
-        # TODO lock all new holes
+        # TODO lock all new holes (if not already working to just lock module)
+
+        # TODO redraw / refresh / updateuserinterface?
+        # these together didn't work, and might have unmarked as modified
+        # (couldn't save directly)
+        #pcbnew.UpdateUserInterface()
+        #pcbnew.Refresh()
 
     # TODO need to explicitly mark board as modified, or do these calls have that built in?
 
 # just since it is not evident there is still support for an action plugins interface
 # no menu in HEAD now
 #HolePlugin().register() # Instantiate and register to Pcbnew
-
-# TODO redraw
 
 h = HolePlugin()
 h.Run()
