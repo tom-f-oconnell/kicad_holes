9eb26b606c8f007ecc1b6d8354463891b3dacc72
Tom O'Connell <toconnel@caltech.edu>
2017-12-13 21:24:23 -0800

    Filtering out Edge.Cuts circles in plugin, now just need to figure out how best to turn them to holes.

--- edges2holes_action.py
+++ edges2holes_action.py
@@ -6,30 +6,34 @@ import pcbnew
 
 class HolePlugin(pcbnew.ActionPlugin):
     def defaults(self):
-		self.name = "Edge.Cuts circles to NPTHs."
+        """
+        """
+        self.name = "Edge.Cuts circles to NPTHs."
         self.category = "convert holes"
         self.description = "Convert circles in Edge.Cuts layer to non-plated through-holes."
-
+ 
     def Run(self):
-        # The entry function of the plugin that is executed on user action
-        print("Hello World")
+        """
+        The entry function of the plugin that is executed on user action
+        """
         board = pcbnew.GetBoard()
 
         # get circles drawn on board
         # TODO maybe include some means of filtering out possible circular board outline
         drawing_list = board.DrawingsList()
-        i = 0
-        while True:
-            # TODO cast from board_item?
-            drawing = drawing_list.Get(i)
-            if drawing is None:
-                break
-            i += 1
-            print drawing.GetCenter()
-            print drawing.IsConnected()
-            print drawing.GetLayerName()
-
-        # TODO option to use nearest hole in existing library option / next largest (report error)
+        for d in drawing_list:
+            if d.GetLayerName() == 'Edge.Cuts':
+                # to gaurd against random stuff being on edge cuts for some reason
+                # causing weird errors
+                assert type(d) == pcbnew.DRAWSEGMENT, 'non-DRAWSEGMENT on Edge.Cuts'
+                if d.GetShapeStr() == 'Circle':
+                    # TODO check these are what i want, and don't need modification
+                    # w/ thickness or whatever (check using dxf import)
+                    radius = d.GetRadius()
+       
+        # TODO option to use nearest hole in existing library option / next largest
+        # (report error)
+        # TODO option to take a set of target hole sizes (for DFM purposes)
 
         # TODO option to make new hole components and put in project specific library
         # TODO make library if necessary...
@@ -38,5 +42,12 @@ class HolePlugin(pcbnew.ActionPlugin):
 
         # TODO lock all new holes
 
-SimplePlugin().register() # Instantiate and register to Pcbnew
+    # TODO need to explicitly mark board as modified, or do these calls have that built in?
+
+# just since it is not evident there is still support for an action plugins interface
+# no menu in HEAD now
+#HolePlugin().register() # Instantiate and register to Pcbnew
+
+h = HolePlugin()
+h.Run()
 
