a1e1daafb92acf9002f68d8361f3885d8305ea7a
Tom O'Connell <toconnel@caltech.edu>
2017-12-13 16:01:24 -0800

    Starting to add pcbnew commands to read drawings.

--- edges2holes_action.py
+++ edges2holes_action.py
@@ -2,6 +2,8 @@
 
 import pcbnew
 
+# TODO how to run this offline too?
+
 class HolePlugin(pcbnew.ActionPlugin):
     def defaults(self):
 		self.name = "Edge.Cuts circles to NPTHs."
@@ -11,6 +13,22 @@ class HolePlugin(pcbnew.ActionPlugin):
     def Run(self):
         # The entry function of the plugin that is executed on user action
         print("Hello World")
+        board = pcbnew.GetBoard()
+
+        # get circles drawn on board
+        # TODO maybe include some means of filtering out possible circular board outline
+        drawing_list = board.DrawingsList()
+        i = 0
+        while True:
+            # TODO cast from board_item?
+            drawing = drawing_list.Get(i)
+            if drawing is None:
+                break
+            i += 1
+            print drawing.GetCenter()
+            print drawing.IsConnected()
+            print drawing.GetLayerName()
+
         # TODO option to use nearest hole in existing library option / next largest (report error)
 
         # TODO option to make new hole components and put in project specific library
